f8bc531c88cc3b1c02acb8194e7681d2a3095b1f
kieronthomas <kieron.thomas321@hotmail.co.uk>
2023-10-10 23:10:27 +0100

    update speed dial

--- src/components/SmallNav/SpeedDialMenu.tsx
+++ src/components/SmallNav/SpeedDialMenu.tsx
@@ -6,54 +6,54 @@ import { BsFillBarChartFill } from "react-icons/bs";
 import { SiAboutdotme } from "react-icons/si";
 
 const actions = [
-  {
-    icon: <FaReact style={{ fontSize: 20 }} />,
-    name: "Tech Stack",
-    link: "/techstack",
-  },
-  {
-    icon: <BsFillBarChartFill style={{ fontSize: 20 }} />,
-    name: "Professional Development",
-    link: "/development",
-  },
-  {
-    icon: <SiAboutdotme style={{ fontSize: 20 }} />,
-    name: "About",
-    link: "/about",
-  },
+    {
+        icon: <FaReact style={{ fontSize: 20 }} />,
+        name: "Tech Stack",
+        link: "/techstack",
+    },
+    {
+        icon: <BsFillBarChartFill style={{ fontSize: 20 }} />,
+        name: "Professional Development",
+        link: "/development",
+    },
+    {
+        icon: <SiAboutdotme style={{ fontSize: 20 }} />,
+        name: "About",
+        link: "/about",
+    },
 ];
 
 const SpeedDialMenu = () => {
-  return (
-    <SpeedDial
-      ariaLabel="SpeedDial basic example"
-      sx={{
-        position: "absolute",
-        bottom: 5,
-        right: 16,
-        "& .MuiFab-primary": {
-          width: 45,
-          height: 45,
-          backgroundColor: "#3E3E3E",
-          "&:hover": { backgroundColor: "#525252" },
-        },
-      }}
-      icon={<SpeedDialIcon />}
-    >
-      {actions.map((action) => (
-        <SpeedDialAction
-          key={action.name}
-          icon={action.icon}
-          tooltipTitle={action.name}
-          onClick={() => {
-            if (action.link) {
-              window.location.href = action.link;
-            }
-          }}
-        />
-      ))}
-    </SpeedDial>
-  );
+    return (
+        <SpeedDial
+            ariaLabel="SpeedDial basic example"
+            sx={{
+                position: "fixed",
+                bottom: "5%",
+                right: "5%",
+                "& .MuiFab-primary": {
+                    width: 45,
+                    height: 45,
+                    backgroundColor: "#3E3E3E",
+                    "&:hover": { backgroundColor: "#525252" },
+                },
+            }}
+            icon={<SpeedDialIcon />}
+        >
+            {actions.map((action) => (
+                <SpeedDialAction
+                    key={action.name}
+                    icon={action.icon}
+                    tooltipTitle={action.name}
+                    onClick={() => {
+                        if (action.link) {
+                            window.location.href = action.link;
+                        }
+                    }}
+                />
+            ))}
+        </SpeedDial>
+    );
 };
 
 export default SpeedDialMenu;
