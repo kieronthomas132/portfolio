7556e9f82234daf4bd56ef94155538ad993f4a12
kieronthomas <kieron.thomas321@hotmail.co.uk>
2023-12-02 14:07:23 +0000

    update old project with new project

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,6 +1,7 @@
 import Logo from "../../assets/Logo.svg";
 import { FaGithub } from "react-icons/fa";
-import { Tooltip } from "@nextui-org/react";
+import {Button, Tooltip} from "@nextui-org/react";
+import Resume from '../../../public/Kieron_Thomas_Resume.pdf'
 const Navbar = () => {
   const navbarOptions = [
     {
@@ -20,21 +21,26 @@ const Navbar = () => {
   return (
     <nav className="bg-[#1A0A2E] h-[80px] font-inter flex flex-col justify-center text-white">
       <div className="flex justify-around items-center p-2">
-        <img src={Logo} className="w-[35px]" alt="logo" />
+        <img src={Logo} className=" w-[35px]" alt="logo" />
         <div>
-          <ul className="flex items-center text-sm gap-[50px] md:gap-[80px]">
+          <ul className="flex items-center gap-[10px] md:gap-[80px]">
             {navbarOptions.map((nav, index) => (
               <li key={index} >
                 <a href={nav?.link}>
                   <h1>{nav?.title}</h1>
                 </a>
-                <a href={nav?.link} target="_blank">
+                <a href={nav?.link} target="_blank" className='hidden md:flex'>
                   <Tooltip content={nav?.label} color="secondary">
                     <h3 className="text-2xl">{nav?.icon}</h3>
                   </Tooltip>
                 </a>
               </li>
             ))}
+            <a href={Resume} download={true}>
+            <Button size='sm' color='secondary' variant='shadow'>
+              Project Resumé
+            </Button>
+            </a>
           </ul>
         </div>
       </div>
