a1cb04429c0bc2d5d24ca901b1540b43ba317fb1
kieronthomas <kieron.thomas321@hotmail.co.uk>
2024-01-01 23:26:01 +0000

    update Connect.tsx

--- src/components/Connect/Connect.tsx
+++ src/components/Connect/Connect.tsx
@@ -5,11 +5,7 @@ const Connect = () => {
     <div id='connect' className="text-white h-full]mt-[30px] md:mt-0 text-start w-[90%] md:w-[80%] font-preahvihear mx-auto">
       <h1>Connect</h1>
       <h4 className="text-sm lg:w-[700px] my-[15px] leading-7">
-        I'm excited about the opportunity to join as a Junior Front-End
-        Developer. I have a strong passion for creating aesthetic and functional
-        web interfaces. If there are any available positions or if you'd like to
-        connect, I'd love to chat!
-      </h4>
+          I'm excited about the opportunity to join as a Junior Front-End Developer. I have a strong passion for creating aesthetic and functional web interfaces. If you're interested and would like to connect, I'd love to chat!      </h4>
       <div className="flex items-center  gap-[15px]">
         <a
           href="mailto: kieron.thomas321@hotmail.co.uk"
