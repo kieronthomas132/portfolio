57662b6f4fd3f79a3a242af3df46c844a217cf74
kieronthomas <kieron.thomas321@hotmail.co.uk>
2024-03-06 00:08:39 +0000

    updated new project (Quizmania)

--- src/components/Projects/Quizmania.tsx
+++ src/components/Projects/Quizmania.tsx
@@ -21,7 +21,7 @@ const Quizmania = () => {
           "z-30 hidden md:flex xl:hidden pr-5 absolute md:left-[8%] bottom-[25%] lg:bottom-[25%]"
         }
         projectDescription={
-          "Quizmania is a React project developed using Vite that brings the excitement of quizzes to your fingertips. With Quizmania, you can challenge yourself with quizzes sourced from the Open Trivia DB API. This project utilizes React Query for efficient data fetching, Context API to manage quiz options such as category, difficulty, and type, and Tailwind CSS for stylish and responsive design. Additionally, NextUI components enhance the user interface with pre-built elements like buttons."
+          "Quizmania is a React project developed using Vite. With Quizmania, you can challenge yourself with quizzes sourced from the Open Trivia DB API. This project utilizes React Query for efficient data fetching, Context API to manage quiz options such as category, difficulty, and type, and Tailwind CSS for stylish and responsive design. Additionally, NextUI components enhance the user interface with pre-built elements like buttons."
         }
       />
     </section>
