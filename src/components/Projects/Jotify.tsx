3d85dda9dcfffbf0b4ea9eff907bf0543b51110a
kieronthomas <kieron.thomas321@hotmail.co.uk>
2024-04-02 15:13:40 +0100

    update project

--- src/components/Projects/Jotify.tsx
+++ src/components/Projects/Jotify.tsx
@@ -1,25 +1,13 @@
 import ProjectsCard from "./ProjectsCard.tsx";
-import arrow from "../../assets/arrows/curvy_arrow.svg";
-import arrow2 from "../../assets/arrows/curvy_arrow_4.svg";
-import arrow_2_light from "../../assets/arrows/arrow_2_light.svg";
-import arrow_light from "../../assets/arrows/arrow_light.svg";
 import jotify from "../../assets/projects/jotify_1.png";
-
 const Jotify = () => {
   return (
     <section>
       <ProjectsCard
-        liveLink={"https://jotify-nine.vercel.app/login"}
         codeLink={"https://github.com/kieronthomas132/Jotify"}
-        projectTitle={"Jotify"}
-        arrow={arrow}
-        arrow2={arrow2}
-        arrow_2_light={arrow_2_light}
-        arrow_light={arrow_light}
+        projectType="Notes Application"
+        projectName={"Jotify"}
         projectImage={jotify}
-        arrow2Position={
-          "z-30 hidden md:flex xl:hidden pr-5 absolute md:left-[8%] bottom-[25%] lg:bottom-[25%]"
-        }
         projectDescription={
           "This is a React application built using Vite for fast development and optimized builds. It provides a simple note-taking interface for users with the added feature of categorizing notes. The application utilizes Appwrite for user authentication, signup, and login functionalities. It also leverages NextUI for UI components, Tanstack Query for efficient data fetching, and Redux for state management."
         }
--- src/components/Projects/ProjectsCard.tsx
+++ src/components/Projects/ProjectsCard.tsx
@@ -1,46 +1,44 @@
-import {IoMdArrowForward} from "react-icons/io";
-import {motion} from "framer-motion";
+import { IoMdArrowForward } from "react-icons/io";
+import { motion } from "framer-motion";
 
 const ProjectsCard = ({
-                          projectImage,
-                          projectName,
-                          projectDescription,
-                          projectType,
-                      }: {
-    projectImage: string;
-    projectName: string;
-    projectDescription: string;
-    codeLink: string;
-    projectType: string;
+  projectImage,
+  projectName,
+  projectDescription,
+  projectType,
+}: {
+  projectImage: string;
+  projectName: string;
+  projectDescription: string;
+  codeLink: string;
+  projectType: string;
 }) => {
-    return (
-        <motion.a
-            whileHover={{
-                scale: 1.05,
-                transition: {type: "spring", stiffness: 200, damping: 10},
-            }}
-            href={`/project/${projectName.toLowerCase()}`}
-            className="w-full md:w-[calc(33.33% - 30px)] lg:w-[calc(33.33% - 30px)] "
-        >
-            <section
-                className="2xl:w-[440px] w-auto mx-auto rounded-t-lg hover:shadow-[#C0A790] hover:shadow-xl border-2 border-[#623D2F]">
-                <div className="bg-[#623D2F] h-[200px] p-10 flex items-center justify-center">
-                    <img className="w-[130px]" src={projectImage} alt={projectName}/>
-                </div>
-                <div className="w-[90%] mx-auto">
-                    <h1 className="text-[17px] mt-4 font-[700]">{projectType}</h1>
-                    <h1 className="text-[25px] mb-4 mt-3 font-[500]">{projectName}</h1>
-                    <p className="font-[400] text-[17px]">{projectDescription}</p>
-                </div>
-                <div className="w-[90%] mx-auto mt-[40px] bg-[#83604F] h-[2px]"/>
-                <div
-                    className="w-[90%] flex items-center justify-between mx-auto mt-[40px] font-[500] mb-[20px] text-[17px]">
-                    <a>View Project</a>
-                    <IoMdArrowForward className="text-[25px]"/>
-                </div>
-            </section>
-        </motion.a>
-    );
+  return (
+    <motion.a
+      whileHover={{
+        scale: 1.05,
+        transition: { type: "spring", stiffness: 200, damping: 10 },
+      }}
+      href={`/project/${projectName.toLowerCase()}`}
+      className="w-full md:w-[calc(33.33% - 30px)] lg:w-[calc(33.33% - 30px)] "
+    >
+      <section className="2xl:w-[440px] w-auto mx-auto rounded-t-lg hover:shadow-[#C0A790] hover:shadow-xl border-2 border-[#623D2F]">
+        <div className="bg-[#623D2F] h-[200px] p-10 flex items-center justify-center">
+          <img className="w-[130px]" src={projectImage} alt={projectName} />
+        </div>
+        <div className="w-[90%] mx-auto">
+          <h1 className="text-[17px] mt-4 font-[700]">{projectType}</h1>
+          <h1 className="text-[25px] mb-4 mt-3 font-[500]">{projectName}</h1>
+          <p className="font-[400] text-[17px]">{projectDescription}</p>
+        </div>
+        <div className="w-[90%] mx-auto mt-[40px] bg-[#83604F] h-[2px]" />
+        <div className="w-[90%] flex items-center justify-between mx-auto mt-[40px] font-[500] mb-[20px] text-[17px]">
+          <a>View Project</a>
+          <IoMdArrowForward className="text-[25px]" />
+        </div>
+      </section>
+    </motion.a>
+  );
 };
 
 export default ProjectsCard;
